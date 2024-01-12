01f39fc1bb330cd7c09717fc04f44622690cb930
Kasualix <670067575@qq.com>
2024-01-12 17:17:55 +0800

    abandon fabric networking api

--- src/main/java/de/dafuqs/revelationary/networking/RevelationaryS2CPacketReceivers.java
+++ src/main/java/de/dafuqs/revelationary/networking/RevelationaryS2CPacketReceivers.java
@@ -3,16 +3,23 @@ package de.dafuqs.revelationary.networking;
 import de.dafuqs.revelationary.ClientRevelationHolder;
 import de.dafuqs.revelationary.RevelationRegistry;
 import de.dafuqs.revelationary.Revelationary;
-import net.fabricmc.networking.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.event.EventNetworkChannel;
 
 public class RevelationaryS2CPacketReceivers {
+	private static final String VERSION = "1.0";
+
+	private static final EventNetworkChannel REVELATION_SYNC_CHANNEL = NetworkRegistry.newEventChannel(RevelationaryPackets.REVELATION_SYNC, () -> VERSION, c -> true, c -> true);
+
 	public static void register() {
-		ClientPlayNetworking.registerGlobalReceiver(RevelationaryPackets.REVELATION_SYNC, (client, handler, buf, responseSender) -> {
+		REVELATION_SYNC_CHANNEL.addListener(networkEvent -> {
+			PacketByteBuf buf = networkEvent.getPayload();
 			try {
 				RevelationRegistry.fromPacket(buf);
 			} catch (Exception e) {
 				Revelationary.logError("Error fetching results from sync packet");
-				e.printStackTrace();
+				Revelationary.logError(e.toString());
 			}
 			ClientRevelationHolder.cloakAll();
 		});
